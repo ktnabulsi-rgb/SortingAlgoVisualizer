35e16ffe5de8f01c0f8acec892dd6e39aa62191c
Owen <Owen_Bowcott@yahoo.com>
2023-05-12 20:45:58 -0700

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        Scanner input = new Scanner(System.in);
+        while(true){
+            System.out.println("Press the corresponding number to see the visualization of the selected sorting algorithm. \n1.Merge Sort\n2.Bubble Sort\n0.Exit");
+            int selection = input.nextInt();
+            if(selection == 0){
+                break;
+            }
+            else if(selection == 1){
+                //Run merge sort
+            }
+            else if(selection == 2){
+                //Run bubble sort
+            }
+        }
+        System.out.println("Have a good day!");
     }
 
     /* Prints the array */
