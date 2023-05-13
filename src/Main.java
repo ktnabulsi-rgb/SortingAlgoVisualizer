b930763b5982b450008e682cd4888debfc02fc8a
Kareem <knabulsi@student.sdccd.edu>
2023-05-12 20:43:25 -0700

    Sorts implementation.

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("test");
     }
+
+    /* Prints the array */
+    public void printArray(int[] arr) {
+            int n = arr.length;
+            for (int i = 0; i < n; ++i) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+    }
 }
---
+++ src/MergeSort.java
@@ -0,0 +1,34 @@
+public class MergeSort {
+    public static void mergeSort(int[] array) {
+        if (array.length < 2) {
+            return;
+        }
+        int mid = array.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[array.length - mid];
+
+        System.arraycopy(array, 0, left, 0, mid);
+        System.arraycopy(array, mid, right, 0, array.length - mid);
+
+        mergeSort(left);
+        mergeSort(right);
+        merge(array, left, right);
+    }
+
+    private static void merge(int[] array, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                array[k++] = left[i++];
+            } else {
+                array[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            array[k++] = left[i++];
+        }
+        while (j < right.length) {
+            array[k++] = right[j++];
+        }
+    }
+}
